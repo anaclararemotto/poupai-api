9a9d93fb16155f9df980d2114bb983842c575666
Clara <clararemotto@gmail.com>
2025-06-16 16:13:31 -0300

    ajuste para conectar com a aplicação

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
-import express from "express"
+import express from "express";
 import conectaNaDatabase from "./config/db.Connect.js";
 import routes from "./routes/index.js";
+import cors from "cors";
 
 const conexao = await conectaNaDatabase();
 
 conexao.on("error", (erro) => {
-    console.error("Erro de conexão", erro);
+  console.error("Erro de conexão", erro);
 });
 
 conexao.once("open", () => {
-    console.log("Conexão com o banco feita com sucesso!");
-    
-})
+  console.log("Conexão com o banco feita com sucesso!");
+});
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:4200", 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+app.use(express.json()); 
+
 routes(app);
+
+app.listen(4000, () => console.log("API rodando na porta 4000"));
 
 export default app;
